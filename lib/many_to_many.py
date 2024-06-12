c73e4948e55ea3ed7b0371dd67dd10e9979a802c
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-12 23:16:46 +0300

    Testing code in Author class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,6 +1,16 @@
 class Author:
-    pass
 
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
 
 class Book:
     pass
