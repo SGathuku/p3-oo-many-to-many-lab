9235ce49d4558da67997ff1d83ea9272acf4c74e
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-12 23:19:41 +0300

    Testing code in Contract class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -33,4 +33,56 @@ class Book:
 
 
 class Contract:
-    pass
+
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception
+        self._author = value
+
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise Exception
+        self._book = value
+
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise Exception
+        self._date = value
+
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise Exception
+        self._royalties = value
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
