c912a36d4853ea57a1e3ed27e52f9eddfce4d5d4
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-12 23:18:51 +0300

    Testing code in Book class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -18,7 +18,18 @@ class Author:
         return sum([contract.royalties for contract in self.contracts()])
 
 class Book:
-    pass
+
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
